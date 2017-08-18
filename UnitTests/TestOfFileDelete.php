e89253b001a112c85956c15b09687d6905a3840d
Bipin Jitiya <bipinjitiya77@gmail.com>
2017-08-19 01:16:14 +0530

    Add files via upload

---
+++ UnitTests/TestOfFileDelete.php
@@ -0,0 +1,18 @@
+<?php
+require_once('simpletest/autorun.php');
+require_once('../fb/unlink_directory.php');
+
+class TestOfFileDelete extends UnitTestCase {
+	public $unlink_directory;
+	public function setUp() {
+		$this->unlink_directory = new unlink_directory();
+	}
+	function testremove_directory( $directory = 'tmp_dir' ) {
+		$this->unlink_directory->remove_directory( $directory );
+		$this->assertFalse(file_exists('tmp_dir'));
+	}
+	public function tearDown() {
+		unset( $this->unlink_directory );
+	}
+}
+?>
---
+++ UnitTests/TestOfZipper.php
@@ -0,0 +1,19 @@
+<?php
+require_once('simpletest/autorun.php');
+require_once('../fb/zipper.php');
+
+class TestOfZipper extends UnitTestCase {
+	public $zipper;
+	public function setUp() {
+	$this->zipper = new zipper();
+	}
+	public function testget_zip( $album_download_directory = 'tmp_download' ) {
+		$actual = $this->zipper->get_zip( $album_download_directory );
+		$this->assertEqual( $actual, '<a href="zip/fb-album_'.date("Y-m-d").'_'.date("H-i-s"). '.zip" id="download-link" class="btn btn-success link-buttons-border-color" >Download Zip Folder</a>' );
+	}
+	public function tearDown() {
+		unset($this->zipper);
+	}
+
+}
+?>

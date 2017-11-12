a756afd0d0213c680d13b056fe0f567b7d4e0bec
Bipin Jitiya <bipinjitiya77@gmail.com>
2017-11-12 22:14:32 +0530

    Update TestOfFileDelete.php

--- UnitTests/TestOfFileDelete.php
+++ UnitTests/TestOfFileDelete.php
@@ -1,5 +1,5 @@
 <?php
-require_once('simpletest/autorun.php');
+//require_once('simpletest/autorun.php');
 require_once(dirname(__FILE__)."/../unlink_directory.php");
 
 class TestOfFileDelete extends UnitTestCase {
